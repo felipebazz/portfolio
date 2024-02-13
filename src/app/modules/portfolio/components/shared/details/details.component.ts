7f7dd9ad4804fbdced028b7b92b4c62f76bdcb79
Felipe Morais <felipebazz@live.com>
2024-02-13 04:02:50 -0300

    alterando de class binding para ngClass

--- src/app/modules/portfolio/components/shared/details/details.component.ts
+++ src/app/modules/portfolio/components/shared/details/details.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, OnInit, signal } from '@angular/core';
 import { EDetailType } from '../../../enums/EDetailType.enum';
 import { IDetails } from '../../../interfaces/details.interface';
 import { StacksAndToolsComponent } from '../stacks/stacks-and-tools.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-details',
     standalone: true,
     templateUrl: './details.component.html',
     styleUrl: './details.component.scss',
-    imports: [StacksAndToolsComponent]
+    imports: [CommonModule, StacksAndToolsComponent]
 })
 export class DetailsComponent implements OnInit {
   @Input({required: true}) public inputDetail: Array<IDetails> = [];
