30698be55546daa339f73491eb5c77d8bfd3c392
Felipe Morais <felipebazz@live.com>
2024-02-17 09:21:56 -0300

    correções nos modelos de experiência e educação

--- src/app/modules/portfolio/models/education-config.model.ts
+++ src/app/modules/portfolio/models/education-config.model.ts
@@ -11,10 +11,11 @@ export class EducationConfig {
         date: 'Universidade Paulista | 2016 - 2018',
       },
       description: `
-        <p>Curso tecnológico em analise e desenvolvimento de sistemas, com foco no desenvolvimento de sistemas utilizando metodologia ágil,
-        gerenciamento de projetos, e boas práticas de programação. Apresentando também ferramentas utéis para o gerenciamento de projetos e ciclo de vida de desenvolvimento.</p>`,
-      stacksIds: [],
-      url: ''
+        <p>
+        Curso tecnológico em análise e desenvolvimento de sistemas, com foco no desenvolvimento de sistemas utilizando metodologia ágil,
+        gerenciamento de projetos, e boas práticas de programação. Apresentando também ferramentas utéis para o gerenciamento de projetos e ciclo de vida de desenvolvimento.
+        </p>`,
+      stacksIds: []
     },
     {
       type: EDetailType.Education,
@@ -23,10 +24,11 @@ export class EducationConfig {
         date: 'ETEC de Francisco Morato | 2012 - 2013',
       },
       description: `
-        <p>Curso técnico em Informática com foco em desenvolvimento de sistemas, introduzindo lógica de programação,
-        apresentando as linguagens de programação, ciclo de vida e todo o ecossistema do desenvolvimento de software.</p>`,
-      stacksIds: [],
-      url: ''
+        <p>
+        Curso técnico em Informática com foco em desenvolvimento de sistemas, introduzindo lógica de programação,
+        apresentando as linguagens de programação, ciclo de vida e todo o ecossistema do desenvolvimento de software.
+        </p>`,
+      stacksIds: []
     }
   ])
 }
